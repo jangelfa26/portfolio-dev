4bc70394664de092502222e5b28c9cfb5ec7d6b4
José Ángel Frade Álvarez <jangelfa26@educastur.es>
2026-04-23 23:53:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ app.use("/portfolio", portfolioRoutes);
 app.get("/", async (req, res) => {
 
     let users = [];
-    try {
-        users = await getAllUsers();    
+   try {
+        users = await getAllUsers();
     } catch (error) {
         console.error(error);
+        return res.status(500).send("Error de base de datos");
     }
     
 
