12ebf16fe9b4438e07de5a3bc05770f42e607245
José Ángel Frade Álvarez <jangelfa26@educastur.es>
2026-04-24 00:56:33 +0200

    Update socialLinks.js

--- models/socialLinks.js
+++ models/socialLinks.js
@@ -1,33 +1,27 @@
 import connection from "../db/connection.js";
 
 export async function getLinks(userId) {
-
-    const [rows] = await connection.execute(
-        "SELECT * FROM social_links WHERE user_id=?",
-        [userId]
-    );
-
-    return rows;
+  const [rows] = await connection.execute(
+    "SELECT * FROM social_links WHERE user_id=?",
+    [userId]
+  );
+  return rows;
 }
 
 export async function createLink(platform, url, userId) {
-
-    await connection.execute(
-        "INSERT INTO social_links(platform,url,user_id) VALUES(?,?,?)",
-        [platform, url, userId]
-    );
-
+  await connection.execute(
+    "INSERT INTO social_links(platform,url,user_id) VALUES(?,?,?)",
+    [platform, url, userId]
+  );
 }
 
 export async function deleteLink(id, userId) {
+  const [result] = await connection.execute(
+    "DELETE FROM social_links WHERE id=? AND user_id=?",
+    [id, userId]
+  );
 
-    const [result] = await connection.execute(
-       "DELETE FROM social_links WHERE id=? AND user_id=?",
-        [id, userId]
-    );
-
-    if (result.affectedRows === 0) {
-        throw new Error("No autorizado o recurso inexistente");
-    }
-
+  if (result.affectedRows === 0) {
+    throw new Error("No autorizado o recurso inexistente");
+  }
 }
