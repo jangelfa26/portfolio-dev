c320d96b59e480a631073a9da308a7d38c63b3b5
José Ángel Frade Álvarez <jangelfa26@educastur.es>
2026-04-24 00:08:36 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,9 @@ host: process.env.DB_HOST,
 user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_NAME
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
