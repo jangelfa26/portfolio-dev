f6aca2e0ed524b36d95c935242c9bdd2169b72f5
José Ángel Frade Álvarez <jangelfa26@educastur.es>
2026-04-24 00:55:43 +0200

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,41 +1,45 @@
 import connection from "../db/connection.js";
 
 export async function createUser(username, password, email) {
-    await connection.execute(
-        "INSERT INTO users(username, password, email, photo) VALUES(?,?,?,?)",
-        [username, password, email, '/default-avatar.jpg']
-    );
+  await connection.execute(
+    "INSERT INTO users(username, password, email, photo) VALUES(?,?,?,?)",
+    [username, password, email, "/default-avatar.jpg"]
+  );
 }
 
 export async function getUserByUsername(username) {
-    const [rows] = await connection.execute(
-        "SELECT * FROM users WHERE username=?",
-        [username]
-    );
-    return rows[0];
+  const [rows] = await connection.execute(
+    "SELECT * FROM users WHERE username=?",
+    [username]
+  );
+  return rows[0];
 }
 
 export async function getUserById(id) {
-    const [rows] = await connection.execute(
-        "SELECT * FROM users WHERE id=?",
-        [id]
-    );
-    return rows[0];
+  const [rows] = await connection.execute(
+    "SELECT * FROM users WHERE id=?",
+    [id]
+  );
+  return rows[0];
 }
 
 export async function getAllUsers() {
-        const [rows] = await connection.execute(
-        "SELECT id, username, bio, photo, email FROM users"
-    );
-    return rows;
+  const [rows] = await connection.execute(
+    "SELECT id, username, bio, photo, email FROM users"
+  );
+  return rows;
 }
 
 export async function updateUser(id, bio, email, photo) {
-    const currentUser = await getUserById(id);
-    const newPhoto = photo || currentUser.photo;
-    
-    await connection.execute(
-        "UPDATE users SET bio=?, email=?, photo=? WHERE id=?",
-        [bio || null, email, newPhoto, id]
-    );
-}   
+  const [current] = await connection.execute(
+    "SELECT photo FROM users WHERE id=?",
+    [id]
+  );
+
+  const currentUser = current[0];
+
+  await connection.execute(
+    "UPDATE users SET bio=?, email=?, photo=? WHERE id=?",
+    [bio || null, email, photo || currentUser.photo, id]
+  );
+}
